036618a0313c0f2c33e18b28d6648e0c977a96ce
gabs2a <gaby2a@hotmail.fr>
2024-06-13 11:01:06 +0200

    Initial Commit

---
+++ ProjetApi/ProjetApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetApiApp.swift
+//  ProjetApi
+//
+//  Created by Ortega Gabriel on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProjetApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
